da2564bc5906565fc66fa1c5679228b42fb3028b
github username <github email address>
2019-01-11 16:34:19 -0800

    cleans up the ratings form helper

--- app/helpers/ratings_helper.rb
+++ app/helpers/ratings_helper.rb
@@ -13,9 +13,18 @@ module RatingsHelper
     when 1
       "⭐️"
     else
-    "No ratings yet. Be the first?  🌠"
+    "No ratings yet. Be the first?"
     end
   end
   
-  
+  def wine_id_field(rating)
+     if rating.wine.nil?
+       select_tag "ratings[:wine_id]", options_from_collection_for_select(Wine.all, :id, :name), :prompt => "Select a wine"
+    else
+      "#{rating.wine.name}"
+      hidden_field_tag :wine_id, rating.wine.id
+    end
+  end
+
+
 end
