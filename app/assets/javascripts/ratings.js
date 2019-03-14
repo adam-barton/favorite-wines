6f22e464a8cb6a0e9965c60d0b50792edef0730f
github username <github email address>
2019-03-13 19:50:45 -0700

    adds an event listener

--- app/assets/javascripts/ratings.js
+++ app/assets/javascripts/ratings.js
@@ -3,9 +3,30 @@ $(document).ready(function() {
 });
 
 function attachEventListeners() {
-    $('#previous').on('click', () => previousGames(event));
+    $('.previous').on('click', () => previousGames());
 }
 
-function previousGames(event) {
+function previousGames() {
     event.preventDefault();
+    console.log(this)
+    alert("Hello!")
 }
+
+// class Rating {
+//     constructor(obj) {
+//         this.id = obj.id
+//         this.user_id = obj.user_id
+//         this.wine_id = obj.wine_id
+//         this.stars = obj.stars
+//         this.comments = obj.comments
+//         this.taste = obj.taste
+//     }
+// }
+
+// Rating.prototype.postHTML = function() {
+//     return { `
+//         <p>${this.stars}</p>
+//         <p>${this.comments}</p>
+//         <p>${this.taste}</p>
+//     ` }
+// }
