5f0b25781b289f9adac2737a56e3fd5cc3ab0034
github username <github email address>
2019-03-17 19:02:01 -0700

    update the users wine list on the users show path

--- app/assets/javascripts/user_wines.js
+++ app/assets/javascripts/user_wines.js
@@ -18,4 +18,42 @@ function getUserWines() {
 function getWineDetails(wine) {
     event.preventDefault();
     console.log(wine)
+    let ratingSpace = $(`#wineDetail-${wine.id}`)
+    ratingSpace.empty();
+
+    $.ajax({
+        url: `/wines/${wine.id}/ratings`,
+        method: 'get',
+        dataType: 'json'
+    }).done(function(response) {
+        for (const rating of response) {
+            if (rating.user.id === parseInt(location.pathname.slice(-1))) {
+                ratingSpace.append(`<p>Stars: ${rating.stars}</p>`)
+            };
+        };
+    });
 }
+
+
+
+// const getWineRatings = (wine) => {
+//     event.preventDefault()
+//     let space = $('.wine-rating-space')
+//     space.empty();
+//     $.ajax({
+//         url: "/wines/" + wine.id + "/ratings",
+//         method: 'get',
+//         dataType: 'json'
+//     }).done(function(response) {
+//         // debugger
+
+//         // ratingSpace.empty();
+//         for (const rating of response) {
+//             let thisRating = new Rating(rating);
+//             let addRating = thisRating.ratingDetails();
+//             let ratingSpace = $(`#wine-ratings-${response[0].wine.id}`)
+//             ratingSpace.append(addRating);
+//             // $(`#wine-ratings-${thisRating.wine_id}`).append(addRating);
+//         }
+//     });
+// } // -- end --
